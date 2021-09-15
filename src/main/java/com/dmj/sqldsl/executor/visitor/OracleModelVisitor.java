af4f00a40431bb45aa742884007a27390ce71707
dunmengjun <a2091013@outlook.com>
2021-09-15 21:53:44 +0800

    修复oracle翻译多了翻页参数的bug

--- src/main/java/com/dmj/sqldsl/executor/visitor/OracleModelVisitor.java
+++ src/main/java/com/dmj/sqldsl/executor/visitor/OracleModelVisitor.java
@@ -2,7 +2,6 @@ package com.dmj.sqldsl.executor.visitor;
 
 import com.dmj.sqldsl.builder.Limit;
 import com.dmj.sqldsl.model.DslQuery;
-import com.dmj.sqldsl.model.SelectFrom;
 import com.dmj.sqldsl.model.column.Column;
 import com.dmj.sqldsl.model.column.SimpleColumn;
 import java.util.ArrayList;
@@ -14,28 +13,18 @@ public class OracleModelVisitor extends StandardModelVisitor {
   private static final String PAGE_COLUMN_ALIAS = "rn";
 
   @Override
-  protected String visit(SelectFrom selectFrom) {
-    List<Column> columns = new ArrayList<>(selectFrom.getColumns());
-    columns.add(
-        SimpleColumn.builder()
-            .name(PAGE_COLUMN_NAME)
-            .alias(PAGE_COLUMN_ALIAS)
-            .build()
-    );
-    selectFrom.setColumns(columns);
-    return super.visit(selectFrom);
-  }
-
-  @Override
   public String visit(DslQuery query) {
-    String selectFromWhere = visitSelectFromWhere(query);
-    return query.getLimit().map(limit ->
-            String.format("select m.* from (%s) m where %s between %s and %s",
-                selectFromWhere,
-                PAGE_COLUMN_ALIAS,
-                limit.getOffset(),
-                limit.getSize() + limit.getOffset()))
-        .orElse(selectFromWhere);
+    query.getLimit().ifPresent(x -> {
+      List<Column> columns = new ArrayList<>(query.getSelectFrom().getColumns());
+      columns.add(
+          SimpleColumn.builder()
+              .name(PAGE_COLUMN_NAME)
+              .alias(PAGE_COLUMN_ALIAS)
+              .build()
+      );
+      query.getSelectFrom().setColumns(columns);
+    });
+    return super.visit(query);
   }
 
   @Override
